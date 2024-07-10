4ac50db9954c438883296450aca551d05ca581c6
yeou <yeou@cess.one>
2024-07-10 17:00:50 +0800

    find extrinsics at block

--- src/utils.rs
+++ src/utils.rs
@@ -4,10 +4,14 @@ pub mod file;
 pub mod ip;
 pub mod str;
 
-use subxt::ext::sp_core::crypto::{
-    AccountId32, Ss58AddressFormat, Ss58AddressFormatRegistry, Ss58Codec,
+use subxt::{
+    blocks::Extrinsics,
+    ext::sp_core::crypto::{AccountId32, Ss58AddressFormat, Ss58AddressFormatRegistry, Ss58Codec},
+    utils::H256,
+    OnlineClient, PolkadotConfig,
 };
 
+use crate::init_api;
 use crate::polkadot::runtime_types::cp_cess_common::Hash;
 
 pub fn get_ss58_address(account_str: &str) -> Result<String, Box<dyn std::error::Error>> {
@@ -33,4 +37,35 @@ pub fn hash_from_string(v: &str) -> Result<Hash, Box<dyn std::error::Error>> {
     let hash_array: [u8; 64] = bytes.try_into()?;
 
     Ok(Hash(hash_array))
+}
+
+pub fn block_hex_string_to_h256(hex: &str) -> H256 {
+    let hex_without_prefix = if let Some(hex_without_prefix) = hex.strip_prefix("0x") {
+        hex_without_prefix
+    } else {
+        hex
+    };
+
+    let decoded = hex::decode(hex_without_prefix).expect("Failed to decode hex string");
+
+    // Ensure the decoded bytes are exactly 32 bytes
+    if decoded.len() != 32 {
+        panic!("Hex string does not have the expected length");
+    }
+
+    let mut hash_array = [0u8; 32];
+    hash_array.copy_from_slice(&decoded);
+
+    H256(hash_array)
+}
+
+pub async fn get_extrinsics_at(
+    block_hash: H256,
+) -> Result<Extrinsics<PolkadotConfig, OnlineClient<PolkadotConfig>>, Box<dyn std::error::Error>> {
+    let api = init_api().await?;
+
+    let block = api.blocks().at(block_hash).await?;
+    let extrinsics = block.extrinsics().await?;
+
+    Ok(extrinsics)
 }
